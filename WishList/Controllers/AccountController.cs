e82baae505aa8b959b01d4033224d04753ddd042
Ruben Birsan <33668828+rubenbirsan@users.noreply.github.com>
2021-08-30 13:45:06 +0200

    add account controller

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
